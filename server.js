04a6cfff810443d0052211e7686c2aa0537c6e15
leonardo-126 <leopaiva2016@hotmail.com>
2024-03-01 14:55:52 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+import app from './src/index.js'
+
+const PORT = 3000
+
+//fazer a conexao
+
+app.listen(PORT, () => {
+      console.log(`Example app listening on port http://localhost:${PORT}`)
+})
